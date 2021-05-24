4317819fbccacd4cc0ff451b0c0640fa2deb8b4a
Sarah Almahmid <hpsarontm@gmail.com>
2021-05-24 16:28:33 +0300

    done with task!

---
+++ story.js
@@ -0,0 +1,6 @@
+let name = "Sarah";
+let age = 23;
+let hobby = "running";
+
+console.log(`Ms. ${name}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`);
+
